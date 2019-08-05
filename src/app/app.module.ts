d82e91bae97d26a1af284543f81a992b4e223b87
Gerad Ryan <Gerardryan1989@gmail.com>
2019-08-05 18:54:10 +0100

    Gallery added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { Camera } from '@ionic-native/camera/ngx';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
@@ -8,16 +9,23 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {ErrorHandler} from 'protractor/built/exitCodes';
+import { IonicStorageModule } from '@ionic/storage';
+import {PhotoService} from './services/photo.service';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, IonicStorageModule.forRoot()],
   providers: [
+    PhotoService,
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+      Camera, { provide: ErrorHandler, useClass: ErrorHandler }
   ],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+
+}
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, forwardRef, Inject, OnInit} from '@angular/core';
+import { PhotoService } from '../services/photo.service';
+import {Camera} from '@ionic-native/camera/ngx';
+
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
 
-  constructor() {}
+export class Tab2Page implements OnInit {
+    constructor(private camera: Camera, @Inject(forwardRef(() => PhotoService))public photoService: PhotoService) {
 
+    }
+
+    ngOnInit() {
+        this.photoService.loadSaved();
+    }
 }
+
+
+
+
+
